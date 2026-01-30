2aab8c1c2431cd513bfd1f5bf0e3859944aa31b6
adil89189-ship-it <adil89189@gmail.com>
2026-01-30 17:38:13 +0500

    Update db.js

--- db.js
+++ db.js
@@ -2,5 +2,16 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const db = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL.includes("render.com")
+    ? { rejectUnauthorized: false }
+    : false
+});
+
+db.on("connect", () => {
+  console.log("✅ DB connected");
+});
+
+db.on("error", (err) => {
+  console.error("❌ DB error", err);
 });
